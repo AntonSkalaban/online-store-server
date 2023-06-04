8237a15546d6c7a816ceb5848437141a214c7346
AntonSkalaban <112729189+AntonSkalaban@users.noreply.github.com>
2023-06-05 00:06:53 +0300

    init: init database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 const PORT = 3000;
+const DB_URL = 'mongodb+srv://antonskalaban99:online-store-password@cluster0.jvo9axa.mongodb.net/'
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 
 const startApp = async () => {
     try{
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         app.listen(PORT, () => console.log(' server start on port ' + PORT));
     } catch (e) {
         console.log(e)
