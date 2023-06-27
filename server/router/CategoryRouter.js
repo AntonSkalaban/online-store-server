3aaaf3692c309d74abc471439cb88f173a8678b9
Anton Skalaban <antonskalaban@MacBook-Air-Anton.local>
2023-06-27 20:29:36 +0300

    delete: delete server folder

--- server/router/CategoryRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import CategoryController from "../controller/CategoryController.js";
-
-const CategoryRouter = new Router();
-
-CategoryRouter.get('/categories', CategoryController.getAll);
-CategoryRouter.post('/categories', CategoryController.create);   
-
-export default CategoryRouter;
